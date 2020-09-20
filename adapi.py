9623876b3e7d8a70bc2615e26ca81525d2547312
igroykt <artofmotion.igroman@gmail.com>
2020-09-20 14:43:56 +0900

    changed ldap data decoding

--- adapi.py
+++ adapi.py
@@ -4,6 +4,7 @@ import sys
 import ldap
 import ldap.sasl
 import ast
+import datetime
 
 class ADApi:
     ldap_server = ""
@@ -35,18 +36,26 @@ class ADApi:
         un = f"{login}@{domain}"
         return str(un)
 
-    def dekodirui_suka(self, blyat):
-        val = str(blyat)
-        val = val.replace("[", "")
-        val = val.replace("b'", "")
-        val = val.replace("']", "")
-        return val
+    def convert_ldaptimestamp(self, timestamp):
+        timestamp = timestamp.split(".")
+        timestamp = str(timestamp[0])
+        year = timestamp[0]+timestamp[1]+timestamp[2]+timestamp[3]
+        month = timestamp[4]+timestamp[5]
+        day = timestamp[6]+timestamp[7]
+        hour = timestamp[8]+timestamp[9]
+        minutes = timestamp[10]+timestamp[11]
+        seconds = timestamp[12]+timestamp[13]
+        readable = f"{hour}:{minutes}:{seconds} {day}-{month}-{year}"
+        if readable:
+            return readable
+        return False
 
     def connect(self):
         try:
             self.ldap_user = self.login2un(self.ldap_user)
             con = ldap.initialize(self.ldap_server)
             con.set_option(ldap.OPT_REFERRALS, 0)
+            con.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
             con.simple_bind_s(self.ldap_user, self.ldap_pass)
             return con
         except ldap.INVALID_CREDENTIALS as e:
@@ -106,18 +115,20 @@ class ADApi:
         if result:
             for data in result:
                 if type(data[1]) is dict:
-                    obj = self.dekodirui_suka(data[1][attribute])
+                    obj = data[1][attribute]
                     return obj
         return False
 
     def get_name(self, con, login):
         username = self.get_data(con, login, "givenName")
+        username = username[0].decode("utf-8")
         if username:
             return username
         return False
 
     def get_fullname(self, con, login):
         userdn = self.get_data(con, login, "distinguishedName")
+        userdn = userdn[0].decode("utf-8")
         userdn = userdn.split(",")
         userdn = userdn[0].split("=")
         userdn = userdn[1]
@@ -127,6 +138,47 @@ class ADApi:
 
     def get_mail(self, con, login):
         mail = self.get_data(con, login, "mail")
+        mail = mail[0].decode("utf-8")
         if mail:
             return mail
+        return False
+
+    def get_description(self, con, login):
+        desc = self.get_data(con, login, "description")
+        desc = desc[0].decode("utf-8")
+        if desc:
+            return desc
+        return False
+
+    def get_created(self, con, login):
+        timestamp = self.get_data(con, login, "whenCreated")
+        timestamp = timestamp[0].decode("utf-8")
+        when = self.convert_ldaptimestamp(timestamp)
+        if when:
+            return when
+        return False
+
+    def get_changed(self, con, login):
+        timestamp = self.get_data(con, login, "whenChanged")
+        timestamp = timestamp[0].decode("utf-8")
+        when = self.convert_ldaptimestamp(timestamp)
+        if when:
+            return when
+        return False
+
+    def get_groups(self, con, login):
+        data = []
+        groups = self.get_data(con, login, "memberOf")
+        groups = list(dict.fromkeys(groups))
+        for group in groups:
+            group = group.decode("utf-8")
+            group = group.split(",")
+            group = group[0].split("=")
+            data.append(group[1])
+        return data
+
+    def get_certs(self, con, login):
+        certs = self.get_data(con, login, "userCertificate")
+        if certs:
+            return certs
         return False
